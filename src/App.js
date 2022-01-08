16c70a9a632a150d82f27bd707eb2f5601bb2680
Georgi Petrov <placefordesign@gmail.com>
2022-01-08 18:05:21 +0200

    adding useContext hooks to cpomponents

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import ThemeToggle from "./components/ThemeToggle";
 import AuthContextProvider from "./contexts/AuthContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import SongList from "./components/SongList";
+import BookContextProvider from "./contexts/BookContext";
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
         <ThemeContextProvider>
+        <BookContextProvider>
           <Navbar />
           <BookList />
           <SongList />
           <ThemeToggle />
+          </BookContextProvider>
         </ThemeContextProvider>
       </AuthContextProvider>
     </div>
