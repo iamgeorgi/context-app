df4834fac16696f5f21ca177d9b6d4ab5c2c1c72
Georgi Petrov <placefordesign@gmail.com>
2022-01-08 18:20:48 +0200

    everything previous cleared

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import BookList from "./components/BookList";
-import Navbar from "./components/Navbar";
-import ThemeToggle from "./components/ThemeToggle";
-import AuthContextProvider from "./contexts/AuthContext";
-import ThemeContextProvider from "./contexts/ThemeContext";
-import SongList from "./components/SongList";
-import BookContextProvider from "./contexts/BookContext";
-
 function App() {
   return (
     <div className="App">
-      <AuthContextProvider>
-        <ThemeContextProvider>
-          <BookContextProvider>
-            <Navbar />
-            <BookList />
-            <SongList />
-            <ThemeToggle />
-          </BookContextProvider>
-        </ThemeContextProvider>
-      </AuthContextProvider>
+      <h1>hello</h1>
     </div>
   );
 }
