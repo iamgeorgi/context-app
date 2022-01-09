7a427c8fdf5bf98dc3c6d82e66598c9a4eacae77
Georgi Petrov <placefordesign@gmail.com>
2022-01-09 13:28:48 +0200

    new components added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import NewBookForm from './components/BookForm';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+import BookContextProvider from './contexts/BookContext';
+
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+     <BookContextProvider>
+        <Navbar />
+        <BookList />
+        <NewBookForm />
+     </BookContextProvider>
     </div>
   );
 }
